08c8e49453d1dfa56096dfbce9463cda41d1e833
LzPedro <pedromanoel@gec.inatel.br>
2019-11-25 22:50:04 -0300

    Added: Null Token validation

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const restify = require('restify')
 const mongoose = require('mongoose')
 const axios = require('axios').default;
+
 var corsMiddleware = require('restify-cors-middleware')
-let token = ''
 var cors = corsMiddleware({
   preflightMaxAge: 5, //Optional
   origins: ['*'],
@@ -19,7 +19,13 @@ server.use(cors.actual);
 server.use(restify.plugins.bodyParser())
 const porta = 8080
 
-mongoose.connect('mongodb+srv://adauto:adauto@cluster0-rven8.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true,useUnifiedTopology: true })
+let token = ""
+const url_login = 'https://ec021-2019-2-av2-auth.herokuapp.com/auth/login'
+const url_token = 'https://ec021-2019-2-av2-auth.herokuapp.com/auth/validateToken '
+const url_mongo = 'mongodb+srv://adauto:adauto@cluster0-rven8.mongodb.net/test?retryWrites=true&w=majority'
+const Meme = require('./meme');
+
+mongoose.connect(url_mongo,{useNewUrlParser: true,useUnifiedTopology: true })
         .then(_=>{
         console.log("Connected to MongoDB")
 })
@@ -30,105 +36,127 @@ mongoose.set('useUnifiedTopology', true);
 
 //LOGIN  
 server.post('/auth/login', async (req, res,next) => {
-    console.log("TRYING TO LOGIN")
-    const url_login = 'https://ec021-2019-2-av2-auth.herokuapp.com/auth/login'
-    const url_token = 'https://ec021-2019-2-av2-auth.herokuapp.com/auth/validateToken '
     let user = req.body.username; 
     let pass = req.body.password; 
-    axios.post(url_login, { "username": user,"password": pass })
-        .then(function(response, data) {
-            data = response.data.token;
-            axios({
-                method: 'post', //you can set what request you want to be
-                url: url_token,
-                headers: {
-                  Token: data
-                }
-              })
-              .then(function(response,data){
-                  //console.log(response.data.token)
-                 // res.send({Token:`${response.data.token}`});
-              })
-        });
     token = await axios.post(url_login, { "username": user,"password": pass })
     token = token.data.token
-    console.log("TOKENZADA: ", token)
     res.send({Token:`${token}`});
 });
 
-const Meme = require('./meme');
-
-
 //CREATE
 server.post('/meme', (req, res, next) => {
-    let memezada = new Meme(req.body)
-    memezada.save().then(memezada=>{
-        res.json(memezada)
-    }).catch(error=>{
-        res.status(400)
-        res.json({message: error.message})
-    })
-    //res.send({mensagem:`Hello World, ${req.params.nome}`});
+    if(!token){
+        res.status(403)
+        res.json({Erro: "Token Não Fornecido"})
+    }
+    else{
+        axios({
+            method: 'post', 
+            url: url_token,
+            headers: {
+                Token: token
+            }
+            }).then(function(response,data){
+                let memezada = new Meme(req.body)
+                memezada.save().then(memezada=>{
+                    console.log(response.data.token)
+                    res.status(201)
+                    res.json(memezada)
+                }).catch(error=>{
+                    res.status(400)
+                    res.json({message: error.message})
+                })
+            })
+        .catch(error => {
+                res.status(401)
+                res.json({Erro: "Token Inválido"})        
+        });
+    }
 });
 
 //READ_ALL
 server.get('/meme', (req, res,next) => {
-    Meme.find().then(memes=>{
-        res.json(memes)
-        return next()
-    })
-    //res.send({mensagem:`Hello World, ${req.params.nome}`});
+    if(!token){
+        res.status(403)
+        res.json({Erro: "Token Não Fornecido"})
+    }
+    else{
+        Meme.find().then(memes=>{
+            res.status(200)
+            res.json(memes)
+            return next()
+        })
+    }
 });
+
 //READ_ONE
 server.get('/meme/:meme_id([0-9a-fA-F]{24})', (req, res,next) => {
-    Meme.findById(req.params.meme_id).then(meme=>{
-        if(meme){
-            res.json(meme)
-        }
-        else{
-            res.status(404)
-            res.json({message: 'not found'})
-        }
-        return next()
-    })
+    if(!token){
+        res.status(403)
+        res.json({Erro: "Token Não Fornecido"})
+    }
+    else{
+        Meme.findById(req.params.meme_id).then(meme=>{
+            if(meme){
+                res.status(200)
+                res.json(meme)
+            }
+            else{
+                res.status(404)
+                res.json({message: 'not found'})
+            }
+            return next()
+        })
+    }
 });
 
 //UPDATE
 server.patch('/meme/:meme_id([0-9a-fA-F]{24})', async (req, res, next) => {
-    try {
-        let id = req.params.meme_id; //Recebendo o valor do id da URL
-        let result = await Meme.findByIdAndUpdate(id, req.body).lean(); //Buscando pessoa por id e atualizando seus dados
-        if (result != null) { //Caso o resultado não seja nulo, quer dizer que encontramos um registro para atulizar e ele foi atualizado
-            let memezada = await Meme.findById(id); //Buscamos o registro atualizado
-            res.status(200)
-            res.json({memezada: memezada})
-        } else {
-            res.status(404) 
-            res.json({result: 'Not Found'})
+    if(!token){
+        res.status(403)
+        res.json({Erro: "Token Não Fornecido"})
+    }
+    else{
+        try {
+            let id = req.params.meme_id; //Recebendo o valor do id da URL
+            let result = await Meme.findByIdAndUpdate(id, req.body).lean(); //Buscando pessoa por id e atualizando seus dados
+            if (result != null) { //Caso o resultado não seja nulo, quer dizer que encontramos um registro para atulizar e ele foi atualizado
+                let memezada = await Meme.findById(id); //Buscamos o registro atualizado
+                res.status(200)
+                res.json({memezada: memezada})
+            } else {
+                res.status(404) 
+                res.json({result: 'Not Found'})
+            }
+        } catch (error) {
+            res.status(400)
+            res.json({message: error})
         }
-    } catch (error) {
-        res.status(400)
-        res.json({message: error})
     }
 });
 
 //DELETE
 server.del('/meme', async (req, res, next) => {
-    try {
-        let id = req.body.id; //Recebendo o valor do id da URL]
-        console.log(id)
-        let result = await Meme.findByIdAndDelete(id);
-
-        if (result != null) { //Caso o resultado não seja nulo, quer dizer que encontramos um registro para excluir e ele foi excluido
-            res.status(200)
-            res.json({message: "Excluido"})
-        } else {
-            res.status(404) 
-            res.json({result: 'Not Found'})
+    if(!token){
+        res.status(403)
+        res.json({Erro: "Token Não Fornecido"})
+    }
+    else{
+        try {
+            let id = req.body.id; //Recebendo o valor do id da URL]
+            console.log(id)
+            let result = await Meme.findByIdAndDelete(id);
+            if (result != null) { //Caso o resultado não seja nulo, quer dizer que encontramos um registro para excluir e ele foi excluido
+                res.status(204)
+                res.json({message: "Excluido"})
+            } else {
+                res.status(404) 
+                res.json({result: 'Not Found'})
+            }
+        } catch (error) {
+            res.status(400)
+            res.json({message: error})
         }
-    } catch (error) {
-        res.status(400)
-        res.json({message: error})
     }
 });
 
